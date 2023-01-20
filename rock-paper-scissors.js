4f2a0ad67bdbb2a64dede9990805e1cd3a900bbe
Johannes <J-Bird@gmx.de>
2023-01-20 17:39:51 +0100

    Modify Game
    Need to rebuild it and separate Functions.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,10 @@
-const scorePlayer = 0;
-const scoreComputer = 0;
-
-function game(pointsPlayer,pointsComputer) {
+//Start function game
+function game() {
 
     for(let i = 1; i<=5; i++) {
         
         let playerSelection = prompt("Choose between Rock, Paper, Scissor");
         let computerSelection = getComputerChoice();
-
-        let pointsComputer;
-        let pointsPlayer;
-        let result;
 
         //Get the computer's choice (random)
         function getComputerChoice() {
@@ -31,10 +25,8 @@ function game(pointsPlayer,pointsComputer) {
                 return `Round ${i}: Even. Nobody wins.`;
             } else if(computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock") {
                 return `Round ${i}: You loose. Paper beats Rock.`;
-                pointsComputer++;
             } else if(computerSelection.toLowerCase() === "scissor" && playerSelection.toLowerCase() === "rock"){
-                return `Round ${i}: You win. Rock beats Scissor.`;  
-                pointsPlayer++;
+                return `Round ${i}: You win. Rock beats Scissor.`;
             }
 
             //Case: Player choose Paper
@@ -42,10 +34,8 @@ function game(pointsPlayer,pointsComputer) {
                 return `Round ${i}: Even. Nobody wins.`;
             } else if(computerSelection.toLowerCase() === "scissor" && playerSelection.toLowerCase() === "paper") {
                 return `Round ${i}: You loose. Scissor beats Paper.`;
-                pointsComputer++;
             } else if(computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "paper"){
                 return `Round ${i}: You win. Paper beats Rock.`;
-                pointsPlayer++;
             }
 
             //Case: Player choose Scissor
@@ -53,22 +43,13 @@ function game(pointsPlayer,pointsComputer) {
                 return `Round ${i}: Even. Nobody wins.`;
             } else if(computerSelection.toLowerCase() === "rock" && playerSelection.toLowerCase() === "scissor") {
                 return `Round ${i}: You loose. Rock beats Scissor.`; 
-                pointsComputer++;
             } else if(computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "scissor"){
                 return `Round ${i}: You win. Scissor beats Paper.`;
-                pointsPlayer++;
             }}
 
-        console.log(playRound(playerSelection, computerSelection, pointsComputer, pointsComputer)); //Test playRound
-    }
-
-    if(pointsComputer > pointsPlayer) {
-        console.log("Final Result: Computer wins.");
-    } else if(pointsComputer < pointsPlayer) {
-        console.log("Final Result: Player wins.");
-    } else {
-        console.log("Final Result: Unbelievable. Nobody wins - it's even.");
+        console.log(playRound(playerSelection, computerSelection)); //Test playRound
+      
     }
 }
 
-game(scorePlayer,scoreComputer);
+game();
