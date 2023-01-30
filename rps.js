19d69c76535117dba1aa43d7fc19081c90a5e8ba
JoKu21 <J-Bird@gmx.de>
2023-01-30 19:08:52 +0100

    Edit rps.js - need to finish playerSelection

--- rps.js
+++ rps.js
@@ -21,27 +21,90 @@ function createComputerSelection() {
     }
 }
 
-//Test CompChoice
-console.log(createComputerSelection());
+//Create PlayerSelection
+function createPlayerSelection () {
+    rock.addEventListener('click', () => {
+        playerSelection = 'rock';
+        console.log(playerSelection);
+        check = 1;
+        playRound(playerSelection, computerSelection);
+    });
 
-//Create playerSelection
-function createPlayerSelection() {
-    if(document.getElementById('rock').addEventListener('click', () => {return playerChoice = 'rock';})) {
-        return playerChoice = 'rock';
-    } else if(document.getElementById('paper').onclick == true) {
-        console.log('paper');
-    } else if(document.getElementById('rock').onclick == true) {
-        console.log('scissor');
+    paper.addEventListener('click', () => {
+        playerSelection = 'paper';
+        console.log(playerSelection);
+        check = 1;
+        playRound(playerSelection, computerSelection);
+    });
+
+    scissor.addEventListener('click', () => {
+        playerSelection = 'scissor';
+        console.log(playerSelection);
+        check = 1;
+        playRound(playerSelection, computerSelection);
+    });
+}
+
+//Play 1 round
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        return `Even! You both picked ${computerSelection}!`;
+    }
+    
+    if(computerSelection === "rock" && playerSelection === "paper"){
+        return "You win! Paper beats Rock!";
+    } else if (computerSelection === "paper" && playerSelection === "scissor"){
+        return "You win! Scissor beats Paper!";
+    } else if(computerSelection === "scissor" && playerSelection === "rock") {
+        return "You win! Rock beats Scissor!";
+    } else if(computerSelection === "rock" && playerSelection === "scissor") {
+        return "You Loose! Rock beats Scissor!";
+    } else if(computerSelection === "paper" && playerSelection === "rock") {
+        return "You Loose! Paper beats Rock!";
+    } else if(computerSelection === "scissor" && playerSelection === "paper"){
+        return "You Loose! Scissor beats Paper!";
     }
 }
 
-console.log(createPlayerSelection());
+//Function gameResult
+function gameResult(playerSelection,computerSelection){
+    if(playRound(playerSelection, computerSelection) === "You win! Paper beats Rock!" || playRound(playerSelection,computerSelection) === "You win! Scissor beats Paper!" || playRound(playerSelection,computerSelection) === "You win! Rock beats Scissor!"){
+        playerScore ++;
+        console.log("Player Score: " + playerScore);
+        return playerScore;
+    }else if (playRound(playerSelection,computerSelection) === `Even! You both picked ${computerSelection}!`){
+        console.log("Tie! No Points!");
+    } else {
+        computerScore ++;
+        console.log("Computer Score: " + computerScore);
+        return computerScore;
+    }
+}
 
-//Adress functions to buttons (Test->console.log())
-rock.addEventListener('click', () => {
-    createPlayerSelection();
-    console.log(playerSelection);
-});
+//Algorithm for game (playing 5 rounds)
+function game(playerScore, computerScore) {
+    //Start loop for 5 rounds
+    for(let i = 1; i <= 5; i++) {
+        playerSelection = createPlayerSelection(); //get PlayerSelection
+        computerSelection = createComputerSelection(); //get ComputerSelection
 
-paper.addEventListener('click', createPlayerSelection());
-scissor.addEventListener('click', createPlayerSelection());
+        console.log(computerSelection); //Test CompChoice
+
+        console.log(`Round: ${i}`);
+        console.log(playRound(playerSelection,computerSelection));
+        }
+
+        gameResult (playerScore,computerScore);
+    }
+
+//Start Game
+game(playerScore, computerScore);
+
+//Find Winnner
+if (playerScore > computerScore) {
+    console.log(`Final Result\nPlayer Points: ${playerScore}\nComputer Points: ${computerScore}\n\nPlayer won!`);
+} else if (playerScore < computerScore) {
+    console.log(`Final Result\nPlayer Points: ${playerScore}\nComputer Points: ${computerScore}\n\nComputer won!`);
+} else {
+    console.log(`Final Result\nPlayer Points: ${playerScore}\nComputer Points: ${computerScore}\n\nEven. Good game both of you!`);
+}
